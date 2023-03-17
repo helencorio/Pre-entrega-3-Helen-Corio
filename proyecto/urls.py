38513ee1b8f688c4781a13464915a79b6b4e681a
Helen Corio <helenncorio@gmail.com>
2023-03-17 19:17:56 -0300

    nombre de templates

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from proyecto.views import inicio
+from proyecto.views import index, profesores, entregables, estudiantes, cursos
 
 
 urlpatterns = [
-    path('', inicio),
+    path('', index),
+    path('profesores/', profesores),
+    path('entregables/', entregables),
+    path('estudiantes/', estudiantes),
+    path('cursos/', cursos),
 ]
--- proyecto/views.py
+++ proyecto/views.py
@@ -3,8 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def inicio(request):
-    return HttpResponse("Esta es la página de inicio")
+def index(request):
+    return render(request, 'proyecto/index.html')
+
+def profesores(request):
+    return render(request, 'proyecto/profesores.html')
+
+def entregables(request):
+    return render(request, 'proyecto/entregables.html')
+
+def cursos(request):
+    return render(request, 'proyecto/cursos.html')
+
+def estudiantes(request):
+    return render(request, 'proyecto/estudiantes.html')
 
 
 
